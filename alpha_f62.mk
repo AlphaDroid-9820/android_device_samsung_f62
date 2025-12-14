2d7e3e52ec0a2a6cd8f562f6b6442ea157be6f0c
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2025-12-14 00:55:25 -0500

    f62: AlphaDroid 16.1 bring-up

--- alpha_f62.mk
+++ alpha_f62.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2023 The alphaOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,11 +27,11 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+## Inherit some common alpha stuff
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_f62
+PRODUCT_NAME := alpha_f62
 PRODUCT_DEVICE := f62
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-E625F
